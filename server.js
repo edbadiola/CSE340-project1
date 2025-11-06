02d3569bef8d5e68bebdd50765f1f02db4f391bc
edbadiola <u000681426@byui.edu>
2025-11-06 12:57:37 +0800

    Connected mynode project to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is connected and Node is running on port ${port}`);
+        });
+    }
+});
+
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
