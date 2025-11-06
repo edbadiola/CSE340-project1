dcee97d6e980b48e61cc6c067aeb9b9c1dfc2961
edbadiola <u000681426@byui.edu>
2025-11-06 13:36:55 +0800

    Added the Get and GetAll endpoint

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    });
+};
+
+const getSingle = async (req, res) => {
+    const userId = new ObjectId(res.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    });
+};
+
+module.exports = {
+    getAll,
+    getSingle
+}
+
