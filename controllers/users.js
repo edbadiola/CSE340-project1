5ed4f401ceb91997b34b787f97850d75b162e35f
edbadiola <u000681426@byui.edu>
2025-11-06 14:09:50 +0800

    fix getSingle

--- controllers/users.js
+++ controllers/users.js
@@ -10,12 +10,21 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(res.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
+    try {
+        const userId = new ObjectId(req.params.id); 
+        const result = await mongodb
+            .getDatabase()
+            .db()
+            .collection('contacts')
+            .find({ _id: userId });
+
+        result.toArray().then((users) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(users);
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Error retrieving user', error: err });
+    }
 };
 
 module.exports = {
