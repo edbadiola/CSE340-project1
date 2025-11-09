40cf6011d59fcb28edcf0ca8b6d02a48a9cf9a3f
edbadiola <u000681426@byui.edu>
2025-11-09 22:29:49 +0800

    fix swagger

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const express = require("express");
-const mongodb = require("./data/database");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-// Parse JSON bodies
-app.use(express.json());
-
-// CORS headers
+app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Z-Key"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept,Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, DELETE, OPTIONS');
+    next();
+    
+});
+app.use('/', require('./routes'));
+
+
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is connected and Node is running on port ${port}`);
+        });
+    }
 });
 
-// Routes
-app.use("/", require("./routes"));
 
-// Initialize DB and start server
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port, () => {
-      console.log(`Database is connected and Node is running on port ${port}`);
-    });
-  }
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });
